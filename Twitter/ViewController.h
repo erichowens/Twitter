e5dc7e6389bf81e6929fd02946c63fe25417372c
Erich Owens <erichowens@fb.com>
2014-10-31 23:17:50 -0700

    Initial Commit

---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Erich Owens on 10/31/14.
+//  Copyright (c) 2014 Erich Owens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by Erich Owens on 10/31/14.
+//  Copyright (c) 2014 Erich Owens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
